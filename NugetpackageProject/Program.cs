d8c4e963e3ed012f4b2eccbd2fd21653cbf3e616
Basavaraj <basavarajm67@gmail.com>
2025-09-01 11:33:12 +0530

    Inital commit

---
+++ NugetpackageProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using SupportingProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetpackageProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum s = new Sum();
+
+            int a = 5, b = 6;
+            int c = s.Add(a, b);
+            Console.WriteLine($"Sum is {c}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SupportingProject/Sum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportingProject
+{
+    public class Sum
+    {
+        public int Add(int a , int b)
+        {
+            return a + b;
+        }
+
+        public long Add(long a, long b)
+        {
+            return a + b;
+        }
+        
+        
+    }
+}
